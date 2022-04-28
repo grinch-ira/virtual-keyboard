2b46287de09bb0ac73bd30fe593c171379e4d4d4
Hryniuk Iryna <grinch.ira@gmail.com>
2022-04-28 15:40:29 +0300

    feat: add keyDown and keyUp function

--- js/Keyboard.js
+++ js/Keyboard.js
@@ -48,16 +48,106 @@ export default class Keyboard {
         "row",
         i + 1,
       ]);
-      rowElement.style.gridTemplateColumns = `repeat(${row.length}, 1fr)`
-      row.forEach((code)=>{
-        const keyObj = this.keyBase.find((key)=>key.code===code);
-        if(keyObj){
+      rowElement.style.gridTemplateColumns = `repeat(${row.length}, 1fr)`;
+      row.forEach((code) => {
+        const keyObj = this.keyBase.find((key) => key.code === code);
+        if (keyObj) {
           const keyButton = new Key(keyObj);
           this.keyButtons.push(keyButton);
-          rowElement.appendChild(keyButton.div)
+          rowElement.appendChild(keyButton.div);
         }
-      })
+      });
     });
-
+    document.onkeydown = this.processKeyDownEvent;
+    document.onkeyup = this.processKeyUpEvent;
+    this.container.onmousedown = this.createCustomEvent;
+    this.container.onmouseup = this.createCustomEvent;
   }
+  processKeyDownEvent = (e) => {
+    const { code, ctrlKey, shiftKey } = e;
+    this.output.focus();
+    const keyObj = this.keyButtons.find((key) => key.code === code);
+    if (keyObj) {
+      if (!e.type && keyObj.isFnKey && keyObj.small === "Shift") {
+        this.shiftKey = true;
+      }
+      if (e.type && keyObj.isFnKey && keyObj.small === "Shift") {
+        this.shiftKey = true;
+      }
+      if (keyObj.small === "Shift") {
+        this.switchUpperCase(true, shiftKey || this.shiftKey);
+      }
+      if (keyObj.code.match(/Caps/) && !this.isCaps) {
+        this.isCaps = true;
+        this.switchUpperCase(true);
+      } else if (keyObj.code.match(/Caps/) && this.isCaps && !e.repeat) {
+        this.isCaps = false;
+        this.switchUpperCase(false);
+      }
+      if (!e.type && keyObj.code.match(/Control/) && keyObj.isFnKey) {
+        this.ctrlKey = true;
+      }
+      if (keyObj.code.match(/Alt/g) && (ctrlKey || this.ctrlKey)) {
+        if (e.type) {
+          e.preventDefault();
+          this.switchLanguage();
+        }
+      }
+      const regexp =
+        /Tab|ArrowLeft|ArrowUp|ArrowDown|ArrowRight|Delete|Backspace|Enter/i;
+      if (
+        (!keyObj.isFnKey && !ctrlKey) ||
+        keyObj.code.match(/Tab|Alt/) ||
+        (!e.type && keyObj.code.match(regexp))
+      ) {
+        if (e.type) {
+          e.preventDefault();
+        }
+        this.fireKeyPress(
+          keyObj,
+          (shiftKey && !this.isCaps) ||
+            (this.shiftKey && !this.isCaps) ||
+            ((shiftKey || this.shiftKey) &&
+              this.isCaps &&
+              keyObj.sub.innerText) ||
+            (this.isCaps && !keyObj.sub.innerText) ||
+            (this.isCaps && (this.shiftKey || shiftKey) && keyObj.sub.innerText)
+            ? keyObj.shift
+            : keyObj.small
+        );
+      }
+      keyObj.div.classList.add("active");
+      this.keyPressed[keyObj.code] = keyObj;
+      if (!e.type) {
+        keyObj.div.addEventListener("mouseleave", this.resetButtonState, {
+          once: true,
+        });
+      }
+    }
+  };
+
+resetButtonState = (e)=>{
+  this.resetPressedButtons(e.target.dataset.code)
+}
+
+
+
+  processKeyUpEvent = ({ code }) => {
+    const keyObj = this.keyPressed[code];
+    delete this.keyPressed[code];
+    if (keyObj) {
+      if (
+        !keyObj.code.match(/Caps/) ||
+        (keyObj.code.match(/Caps/) && this.isCaps)
+      ) {
+        keyObj.div.classList.remove("active");
+      }
+      if (keyObj.isFnKey && keyObj.small === "Shift" && !this.isCaps) {
+        this.switchUpperCase(false);
+      } else if (keyObj.isFnKey && keyObj.small === "Shift" && this.isCaps) {
+        this.shiftKey = false;
+        this.switchUpperCase(false, true);
+      }
+    }
+  };
 }
